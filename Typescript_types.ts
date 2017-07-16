0259b29c9a33ce78fb95200f9701293ccf397586
venkatsgithub1 <venkatsgithub@gmail.com>
2017-07-16 21:59:33 +0530

    Create Typescript_types.ts
    Types that can be used in Typescript

---
+++ Typescript_types.ts
@@ -0,0 +1,10 @@
+let someString:String;
+let someNumber:number;
+let trueOrFalse:boolean;
+let someArray:Array<any>;
+let someElement:Element;
+let someObject:object;
+let nullThing:null;
+let undefinedThing:undefined;
+let voidThing:void;
+let anyThing:any;
