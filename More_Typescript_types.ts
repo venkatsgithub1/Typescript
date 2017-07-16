dc47a009139d61644d4e8e5142b9daa18e51c4a5
venkatsgithub1 <venkatsgithub@gmail.com>
2017-07-16 22:21:53 +0530

    Create More_Typescript_types.ts
    Some more examples of types in Typescript.

---
+++ More_Typescript_types.ts
@@ -0,0 +1,12 @@
+let someBoolean:boolean=false;
+console.log(someBoolean);
+
+let hex:number=0x0a;
+let binary:number=0b0110;
+let octal:number=0o744;
+
+let color:string="red";
+color="blue";
+
+let fullname:string="John Doe";
+let sentence:string=`Full name is ${fullname}`;
